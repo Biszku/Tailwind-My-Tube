87c50d08530f7bc50476c1a351d17bb00f6e214b
Biszku <dbichu@onet.pl>
2023-12-09 11:22:27 +0100

    done searchbar logic for icons

--- src/components/header/searchContainer/searchContainer.tsx
+++ src/components/header/searchContainer/searchContainer.tsx
@@ -1,11 +1,48 @@
 import { IoMdMic } from "react-icons/io";
 import { HiMagnifyingGlass } from "react-icons/hi2";
+import { FaKeyboard } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
+import { useState, useRef } from "react";
+
 const SearchContainer = () => {
+  const [firstLoad, setFirstLoad] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <div className="flex items-center text-5xl text-white">
       <div className="flex">
-        <div className="flex items-center">
-          <input className="text-2xl " type="text" placeholder="Szukaj" />
+        <div className="flex items-center border-t border-b border-l">
+          {isActive && <HiMagnifyingGlass />}
+          <input
+            ref={inputRef}
+            className="outline-none text-2xl bg-transparent"
+            type="text"
+            placeholder="Szukaj"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            onFocus={() => {
+              setIsActive(true);
+            }}
+            onBlur={() => {
+              setIsActive(false);
+            }}
+            onClick={() => {
+              setFirstLoad(false);
+            }}
+          />
+          {!firstLoad && <FaKeyboard />}
+          {searchText.length > 0 && (
+            <IoMdClose
+              className="hover:cursor-pointer"
+              onClick={() => {
+                setSearchText("");
+                inputRef.current && inputRef.current.focus();
+              }}
+            />
+          )}
         </div>
         <div className="flex items-center">
           <HiMagnifyingGlass />
