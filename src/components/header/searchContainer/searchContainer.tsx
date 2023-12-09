910096adbac9cdedc36fc722e43686270314cae7
Biszku <dbichu@onet.pl>
2023-12-09 12:49:02 +0100

    search bar style done

--- src/components/header/searchContainer/searchContainer.tsx
+++ src/components/header/searchContainer/searchContainer.tsx
@@ -10,13 +10,17 @@ const SearchContainer = () => {
   const [searchText, setSearchText] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <div className="flex items-center text-5xl text-white">
-      <div className="flex">
-        <div className="flex items-center border-t border-b border-l">
-          {isActive && <HiMagnifyingGlass />}
+    <div className="w-1/3 flex items-center justify-end gap-6 text-white py-3">
+      <div className="flex flex-1">
+        <div
+          className={`flex flex-1 items-center border-t border-b border-l  ${
+            isActive ? "border-blue-500 border-r" : "border-gray-700"
+          } rounded-l-full py-3 pl-6 pr-4 gap-1`}
+        >
+          {isActive && <HiMagnifyingGlass className="text-3xl mr-4" />}
           <input
             ref={inputRef}
-            className="outline-none text-2xl bg-transparent"
+            className="flex-1 outline-none text-2xl bg-transparent "
             type="text"
             placeholder="Szukaj"
             value={searchText}
@@ -33,10 +37,10 @@ const SearchContainer = () => {
               setFirstLoad(false);
             }}
           />
-          {!firstLoad && <FaKeyboard />}
+          {!firstLoad && <FaKeyboard className="text-3xl" />}
           {searchText.length > 0 && (
             <IoMdClose
-              className="hover:cursor-pointer"
+              className="text-3xl hover:cursor-pointer"
               onClick={() => {
                 setSearchText("");
                 inputRef.current && inputRef.current.focus();
@@ -44,12 +48,12 @@ const SearchContainer = () => {
             />
           )}
         </div>
-        <div className="flex items-center">
-          <HiMagnifyingGlass />
+        <div className="flex items-center bg-zinc-800 rounded-r-full px-8">
+          <HiMagnifyingGlass className="text-4xl" />
         </div>
       </div>
-      <div className="">
-        <IoMdMic />
+      <div className="grid px-3 place-items-center self-stretch bg-zinc-800  rounded-full ">
+        <IoMdMic className="text-4xl" />
       </div>
     </div>
   );
