324f177b3cf3026e8056a5fa6024371731b49714
Biszku <dbichu@onet.pl>
2023-12-15 21:22:49 +0100

    style: change padding

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import SearchContainer from "./searchContainer/searchContainer";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex items-center justify-between col-span-2 px-10 ">
+    <header className="sticky top-0 flex items-center justify-between col-span-2 px-[2.8rem] ">
       <LogoContainer />
       <SearchContainer />
       <RigthContainer />
