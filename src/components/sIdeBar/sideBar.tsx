f13e08d9af08187b25041870d802ad665ec42151
Biszku <dbichu@onet.pl>
2023-12-16 17:51:15 +0100

    style: added element aside

--- src/components/sIdeBar/sideBar.tsx
+++ src/components/sIdeBar/sideBar.tsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
   return (
-    <aside className="asideElementWidth  sticky top-[56px] text-white">
+    <aside className="asideElementWidth sticky top-[54.25px] text-white pt-[1.4rem] w-[26.5rem]">
       <p>Side Bar</p>
     </aside>
   );
