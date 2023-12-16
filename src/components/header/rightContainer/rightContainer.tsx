ca42227c5966a615fe21e49add37d3db3b3e0f02
Biszku <dbichu@onet.pl>
2023-12-16 17:49:59 +0100

    style: added hover effect

--- src/components/header/rightContainer/rightContainer.tsx
+++ src/components/header/rightContainer/rightContainer.tsx
@@ -4,10 +4,10 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 const RigthContainer = () => {
   return (
     <div className="text-white flex items-center gap-[2.9rem]">
-      <BsCameraVideo className="text-4xl" />
-      <IoMdNotificationsOutline className="text-4xl" />
+      <BsCameraVideo className="text-4xl hover:cursor-pointer" />
+      <IoMdNotificationsOutline className="text-4xl hover:cursor-pointer" />
       <img
-        className="w-12 h-auto rounded-full"
+        className="w-12 h-auto rounded-full hover:cursor-pointer"
         src="/public/avatar.jpg"
         alt="profile"
       />
