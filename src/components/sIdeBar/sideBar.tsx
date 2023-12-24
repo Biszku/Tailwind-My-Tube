0519e2124511a6e4a78ca07c9bf3bd868d8c2c77
Biszku <dbichu@onet.pl>
2023-12-24 22:03:57 +0100

    sidebar navigation added

--- src/components/sIdeBar/sideBar.tsx
+++ src/components/sIdeBar/sideBar.tsx
@@ -1,7 +1,9 @@
+import SideBarMainMenu from "./sideBarMainMenu";
+
 const Sidebar = () => {
   return (
-    <aside className="asideElementWidth sticky top-[54.25px] text-white pt-[1.4rem]">
-      <p>Side Bar</p>
+    <aside className="asideElementWidth sticky top-[54.25px] text-white pt-[1.4rem] ">
+      <SideBarMainMenu />
     </aside>
   );
 };
