c2d48f2219c95e33cd18bbe0bb35cd4e332273d2
Biszku <dbichu@onet.pl>
2023-12-25 21:27:57 +0100

    added element to aside element

--- src/components/sIdeBar/sideBar.tsx
+++ src/components/sIdeBar/sideBar.tsx
@@ -1,9 +1,13 @@
-import SideBarMainMenu from "./sideBarMainMenu";
+import SideBarFirstOptions from "./SideBarFirstOptions";
+import SideBarSecondOptions from "./SideBarSecondOptions";
+import ExploreSection from "./exploreSection";
 
 const Sidebar = () => {
   return (
-    <aside className="asideElementWidth sticky top-[54.25px] text-white pt-[1.4rem] ">
-      <SideBarMainMenu />
+    <aside className="asideElementWidth sticky top-[54.25px] text-white w-[21.5rem]">
+      <SideBarFirstOptions />
+      <SideBarSecondOptions />
+      <ExploreSection />
     </aside>
   );
 };
