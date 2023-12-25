d8efa383e868ac71b021468deace15cb81a567dc
Biszku <dbichu@onet.pl>
2023-12-25 21:28:12 +0100

    changed icon to better

--- src/components/header/rightContainer/rightContainer.tsx
+++ src/components/header/rightContainer/rightContainer.tsx
@@ -1,10 +1,10 @@
-import { BsCameraVideo } from "react-icons/bs";
+import { MdOutlineVideoCall } from "react-icons/md";
 import { IoMdNotificationsOutline } from "react-icons/io";
 
 const RigthContainer = () => {
   return (
     <div className="text-white flex items-center gap-[2.9rem]">
-      <BsCameraVideo className="text-4xl hover:cursor-pointer" />
+      <MdOutlineVideoCall className="text-4xl hover:cursor-pointer" />
       <IoMdNotificationsOutline className="text-4xl hover:cursor-pointer" />
       <img
         className="w-12 h-auto rounded-full hover:cursor-pointer"
