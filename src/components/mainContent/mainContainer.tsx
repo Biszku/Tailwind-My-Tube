4785b67d04b14dfd7b4bbb04872457dc0a4ec0bf
Biszku <dbichu@onet.pl>
2023-12-27 18:26:54 +0100

    created videos container and added some dummy elements

--- src/components/mainContent/mainContainer.tsx
+++ src/components/mainContent/mainContainer.tsx
@@ -1,11 +1,22 @@
 import MainContentNav from "./mainContentNav/mainContentNav";
 
 const MainContainer = () => {
+  const data = new Array(90).fill("Title");
   return (
-    <section className="flex-1 min-w-[0px]">
+    <section className="flex flex-col gap-[2.2rem] flex-1 min-w-[0px]">
       <MainContentNav />
-      <div className="h-[1000vh] ">
-        <p className="text-white">Main Content</p>
+      <div className="grid gap-x-[2rem] grid-cols-[repeat(auto-fit,minmax(31rem,1fr))] pr-[1.6rem]">
+        {data.map((el) => (
+          <div>
+            <div className="aspect-[16/8.6]">
+              <img
+                className="w-[100%] h-[100%] rounded-[1rem]"
+                src="/public/dummyimage.jpg"
+              />
+            </div>
+            <p className="text-white text-[1.6rem]">{el}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
